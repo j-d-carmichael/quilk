d815fbd9c845b8220354b6e0637d0df58c0e7188
John Carmichael <john@johnine.org>
2020-04-11 14:02:19 +0200

    chore: version checking

--- bin/quilk.js
+++ bin/quilk.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 
-require('../lib/quilk.js');
+require('../main.js')
---
+++ main.js
@@ -0,0 +1,14 @@
+const versionCheck = require('npm-tool-version-check').default
+
+const thisVersion = require('./package.json').version // Update this to work for your package
+const remoteJson = 'https://raw.githubusercontent.com/jdcrecur/quilk/master/package.json' // Update this URL to work for your package
+const packageName = 'quilk' // Update this to your package name
+
+console.log('Current quilk version: ' + thisVersion)
+versionCheck(thisVersion, remoteJson, packageName)
+  .then(() => {
+    require('./lib/quilk')
+  })
+  .catch(() => {
+    process.exit(0)
+  })
