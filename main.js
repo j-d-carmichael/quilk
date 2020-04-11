9ffc6216c8aaaf69cb24dac00371469ee6d2b203
John Carmichael <john@johnine.org>
2020-04-11 14:06:56 +0200

    fix: repo url update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const versionCheck = require('npm-tool-version-check').default
 
 const thisVersion = require('./package.json').version // Update this to work for your package
-const remoteJson = 'https://raw.githubusercontent.com/jdcrecur/quilk/master/package.json' // Update this URL to work for your package
+const remoteJson = 'https://raw.githubusercontent.com/johndcarmichael/quilk/master/package.json' // Update this URL to work for your package
 const packageName = 'quilk' // Update this to your package name
 
 console.log('Current quilk version: ' + thisVersion)
